269b61f99e7ce019be9cc33bc8f2d53a0d493d84
byObin <96936141+byObin@users.noreply.github.com>
2023-01-26 07:32:58 +0900

    format 함수

---
+++ format04.py
@@ -0,0 +1,16 @@
+output0 = "{:+d}".format(23) # 정수 출력
+output1 = "{:+7d}".format(23) # 기호 숫자에 붙이기
+output2 = "{:+7d}".format(-23) # 기호 숫자에 붙이기
+output3 = "{:=+7d}".format(23) # 기호 맨앞에 붙이기
+output4 = "{:=+7d}".format(-23) # 기호 맨앞에 붙이기
+output5 = "{:+07d}".format(23) # 기호 붙이고 0으로 채우기
+output6 = "{:+07d}".format(-23) # 기호 붙이고 0으로 채우기
+
+print(output0)
+print(output1)
+print(output2)
+print(output3)
+print(output4)
+print(output5)
+print(output6)
+
